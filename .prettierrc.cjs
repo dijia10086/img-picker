e8597156ecb557ad8270e8d9da1fb9db75063b39
鲁举佩 <653390161@qq.com>
2023-06-09 11:22:54 +0800

    chore: add eslint & prettier

---
+++ .prettierrc.cjs
@@ -0,0 +1,9 @@
+module.exports = {
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  singleQuote: true,
+  semi: true,
+  trailingComma: 'none',
+  bracketSpacing: true // 对象大括号直接是否有空格，默认为 true，效果：{ a: 1 }
+};
